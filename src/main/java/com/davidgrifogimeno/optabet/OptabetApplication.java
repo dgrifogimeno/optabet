79b9f7dabcd817118d901185040d9867a8199cdc
dgrifogimeno <david.grifo.gimeno@gmail.com>
2023-10-24 20:01:33 +0200

    First commit

---
+++ src/main/java/com/davidgrifogimeno/optabet/OptabetApplication.java
@@ -0,0 +1,13 @@
+package com.davidgrifogimeno.optabet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptabetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptabetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidgrifogimeno/optabet/OptabetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidgrifogimeno.optabet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptabetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
