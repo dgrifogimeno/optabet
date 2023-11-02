1f4b2e2cc3aa4847233facf1669b33d6d6dd51f8
dgrifogimeno <david.grifo.gimeno@gmail.com>
2023-11-02 23:22:03 +0100

    CORS policy

--- src/main/java/com/davidgrifogimeno/optabet/controllers/TeamController.java
+++ src/main/java/com/davidgrifogimeno/optabet/controllers/TeamController.java
@@ -1,6 +1,7 @@
 package com.davidgrifogimeno.optabet.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.davidgrifogimeno.optabet.models.Team;
 import com.davidgrifogimeno.optabet.services.TeamService;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping
 public class TeamController {
@@ -16,6 +19,13 @@ public class TeamController {
     @Autowired
     TeamService teamService;
 
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping()
+    public ArrayList<Team> getAllTeams() {
+        return teamService.getAllTeams();
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/totalGoals")
     public int getTotalGoals(@RequestParam (name = "teamId") Long teamId) {
         return teamService.getTotalGoals(teamId);
